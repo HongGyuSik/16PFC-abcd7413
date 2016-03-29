23970f735cb24a10f2627fecf3aaa6f7e881344f
HongGyuSik <abcd7413@naver.com>
2016-03-29 11:51:50 +0900

    ex05 내용입력

---
+++ ex05/ex05.py
@@ -0,0 +1,20 @@
+name = 'GyuSik Hong'
+age = 23 # not a lie
+height_cm = 173
+cm_to_inch = 1.0 / 2.54
+height_inch = height_cm * cm_to_inch
+weight_kg = 65
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+# place holder
+print "Let's talk about %s." % name
+print "He's %g cent imeters tall." % height_cm
+print "He's %g inches tall." % height_inch
+print "He's %d kilograms heavy." % weight_kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height_cm, weight_kg, age + height_cm + weight_kg)
