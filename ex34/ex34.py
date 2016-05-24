5c81b72284edeef25c916cd5077b6d8492df106a
P.sangmin <orbird@naver.com>
2016-05-24 10:22:53 +0900

    added ex34.py 주석입력

--- ex34/ex34.py
+++ ex34/ex34.py
@@ -1,12 +1,12 @@
 # -*-coding:utf8
 
-animals = ['곰', '호랑이', '펭귄', '얼룩말']
-bear = animals[0]
-tiger = animals[1]
-penguin = animals[2]
-zebra = animals[3]
+animals = ['곰', '호랑이', '펭귄', '얼룩말'] # 동물 ='곰', '호랑이', '퓅귄', '얼룩말'
+bear = animals[0] #곰=동물[0]
+tiger = animals[1] #호랑이=동물[1]
+penguin = animals[2] #퓅귄=동물[2]
+zebra = animals[3] #얼룩말=동물[3]
 
-print("bear = %s" % bear)
-print("tiger = %s" % tiger)
-print("penguin = %s" % penguin)
-print("zebra = %s" % zebra)
+print("bear = %s" % bear) #곰을 출력하라
+print("tiger = %s" % tiger) #호랑이를 출력하라
+print("penguin = %s" % penguin) #퓅귄을 출력하라
+print("zebra = %s" % zebra) #얼룩말을 출력하라
